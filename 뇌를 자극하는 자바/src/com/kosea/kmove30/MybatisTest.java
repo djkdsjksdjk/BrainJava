f88d90d7105cec61fa94ddad641376a41ee6cccd
kosea102 <kosea102@LMH>
2018-06-08 09:44:09 +0900

    Mybatis - CRUD 18-06-07

--- 뇌를 자극하는 자바/src/com/kosea/kmove30/MybatisTest.java
+++ 뇌를 자극하는 자바/src/com/kosea/kmove30/MybatisTest.java
@@ -24,12 +24,62 @@ public class MybatisTest {
 		 // Blog blog = session.selectOne("org.mybatis.example.BlogMapper.selectBlog", 101);
 			//Member member = new Member();
 			
-		Member member = session.selectOne("org.mybatis.example.SelectMapper.selectMember", 101);
+			////조회 시작////
+			
+		/*Member member = session.selectOne("org.mybatis.example.SelectMapper.selectMember", 105);
 		
 		System.out.println("회원 아이디: " + member.getId());
+		System.out.println("회원 비밀번호: " + member.getPass());
+		System.out.println("회원 번호: " + member.getMno());*/
+			
+			////조회종료////
 		
+		///////추가시작///////
+		
+		//Member newMember = new Member(102, "kosea2", "12345");
+		//Member newMember = new Member();
+		
+		//newMember.setMno(105);
+		//newMember.setId("TestID5");
+		//newMember.setPass("123456");
+		
+			///////추가종료///////
+		
+		//int insertCount = session.insert("org.mybatis.example.SelectMapper.insertMember", newMember);
+		
+		//System.out.println("추가 건수: " + insertCount);
+		
+		///////////수정시작//////////
+			
+			String queryMapping = "org.mybatis.example.SelectMapper.updateMember";
+			Member member = new Member();
+			member.setMno(105);
+			member.setId("aLEEM");
+			member.setPass("bba6268");
+			
+			//Member member = new Member(105, "LEEID", "123456pw");
+			int updateCount = session.update(queryMapping, member);
+			System.out.println("수정 건수: " + updateCount);
+			
+			
+			
+	///////////수정종료//////////
+		
+		
+		
+		
+		/////////////////////////////삭제////////////////////////////////
+		
+		//"org.mybatis.example.SelectMapper.deletMember", 101
+		
+		//int deleteCount = session.delete("org.mybatis.example.SelectMapper.deleteMember", 101);				
+		//System.out.println("삭제건수: " + deleteCount);
 	
-		} finally {
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			session.commit();
 		  session.close();
 		}
 		
