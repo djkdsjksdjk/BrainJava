dfc725d4f7f138410781f22464af87273aaaa2df
kosea102 <kosea102@LMH>
2018-06-11 17:58:09 +0900

    미니 프로젝트 0611

--- 뇌를 자극하는 자바/src/Test/TestScore.java
+++ 뇌를 자극하는 자바/src/Test/TestScore.java
@@ -1,42 +1,73 @@
 package Test;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
-public class TestScore {
-	public static void main(String[] args, Container contentPane) {
+public  class  TestScore {
+	public  static  void  main ( String [] args , Container  contentPane, Container panel ) {
 	
-	JFrame frame = new JFrame("성적 계산기");
-	frame.setLocation(500, 400);
+	JFrame frame =  new  JFrame ( "성적계산기" );
+	frame.setLocation (500, 400);
 	
 	
 	Container contentpane = frame.getContentPane();
 	String colName[] = {"과목", "학점", "평점"};
+	Integer[] scoreCuts = new Integer[]
+			{95, 90, 85, 80, 75, 70, 65, 60, 0};
+	String[] grades = new String[] {"A+", "A0", 
+			"B+", "B0", "C+", "C0", "D+", "D0", "F"};
+	String score[] = {"선택", "A+", "A0", "B+", "B0",
+			          "C+", "C0", "D+","D0","F"};
 	
-	FlowLayout layout = new FlowLayout();
-	contentPane.setLayout(layout);
+	FlowLayout layout =  new  FlowLayout ();
+	contentPane.setLayout (layout);
 	
-	JTextField text1 = new JTextField(20);
-	JTextField text2 = new JTextField(10);
+	JTextField text1 =  new  JTextField(20);
+	JTextField text2 =  new  JTextField(10);
+	JComboBox<String>scoreCombox = new JComboBox<String>(score);
 	
-	JButton okBtn     = new JButton("계산");
-	okBtn.addActionListener(new TestScore_Listener(Text1, Text2));
+	JButton adBtn = new JButton("칸추가");
 	
-	contentpane.add(text1);
-    contentpane.add(Text2);
-    contentpane.add(okBtn);
+	
+	
+	contentpane.add (text1);
+    contentpane.add (text2);
+    contentpane.add (adBtn);
     
+    panel.add(new JLabel("과목"));
+    panel.add(text1);
+    panel.add(new JLabel("학점"));
+    panel.add(text2);
+    panel.add(new JLabel("평점"));
+    panel.add(scoreCombox);
+    
+    contentPane.add(panel, BorderLayout.NORTH);
+    
+    JButton okBtn      =  new  JButton ("계산");
+	okBtn.addActionListener( new TestScore_Listener ( text1 , text2 ));
+    
+    
+	contentpane.add (okBtn);
+	
+	String[] labels = new String[] {"학점", "총 평점"};
+	
+	Object[][] tables = new Object[][] {scoreCuts, grades};
+	
+	System.out.println("학점 평가를 시작합니다.");
 	
 	
 	
-}
-	
-
+	contentPane.add(panel, BorderLayout.SOUTH);
+    
+	}
 }
--- 뇌를 자극하는 자바/src/Test/TestScore_Listener.java
+++ 뇌를 자극하는 자바/src/Test/TestScore_Listener.java
@@ -1,5 +1,62 @@
 package Test;
 
-public class TestScore_Listener {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+public class TestScore_Listener implements ActionListener {
+	
+	JTextField text1;
+	JTextField text2;
+	
+	
+
+	public TestScore_Listener(JTextField text1, JTextField text2) {
+		super();
+		this.text1 = text1;
+		this.text2 = text2;
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		JButton b= (JButton)e.getSource();
+		if(b.getText().equals("계산")) {
+			
+			//계산
+			
+			String strNum1 = text1.getText();
+			String strNum2 = text2.getText();
+			
+			int num1 = Integer.parseInt(strNum1);
+			int num2 = Integer.parseInt(strNum2);
+			int total = num1 + num2;
+			
+			System.out.println(total);
+			
+			
+		}
+		public void insertTable(String[] arr) throws Exception {
+			
+			String name = arr[0]; //과목 이름
+			String credit = arr[1]; //학점
+			String Score = arr[2]; // 평점
+			
+		}
+		Score = Score.equals("A+", "A", "B+", "B", "C+", "C", "D+", "D", "F");
+		
+
+	}
+
+
+
+	public void insertTable(String[] arr) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
