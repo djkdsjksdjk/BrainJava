76e42c34d66c648be3dbd3f25c40efa7302e5ae4
kosea102 <kosea102@LMH>
2018-06-12 18:58:17 +0900

    0612 - 미니프로젝트 수정

--- 뇌를 자극하는 자바/src/Test/TestScore2.java
+++ 뇌를 자극하는 자바/src/Test/TestScore2.java
@@ -1,68 +1,140 @@
 package Test;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import Test.TestScore_Listener;
 
 public class TestScore2 {
 
-	public static void main(String[] args, Container panel) {
-		JFrame frame = new JFrame("학점 평균 계산 프로그램");
-		frame.setLocation(500, 400);
+	public static void main(String[] args) {
 		
-		Container contentpane = frame.getContentPane();
+		TestScore_Listener testScore = new TestScore_Listener();
+		
+		JFrame frame = new JFrame("학점 평균 계산 프로그램");
+		frame.setPreferredSize(new Dimension(320, 250));
+		frame.setLocation(350, 200);
+		
+		Container contentPane = frame.getContentPane();
 		String colName[] = {"과목", "학점", "평점"};
+		String colName2[] = {"총 학점", "평점"};
 		String score[] = {"선택", "A+", "A0", "B+", "B0",
 		          "C+", "C0", "D+","D0","F"};
 		
-		FlowLayout layout = new FlowLayout();
-		contentpane.setLayout(layout);
+		DefaultTableModel model = new DefaultTableModel(colName, 0);
+		DefaultTableModel model2 = new DefaultTableModel(colName2, 0);
+		JTable table = new JTable(model);
 		
-		JTextField text1 =  new  JTextField(20);
-		JTextField text2 =  new  JTextField(10);
+		GridLayout layout = new GridLayout();
+		contentPane.setLayout(layout);
+		//contentPane.add(new JScrollPane(table), BorderLayout.CENTER);
+		JPanel panel = new JPanel();
+		
+		
+		
+		JTextField text1 =  new  JTextField(8);
+		JTextField text2 =  new  JTextField(2);
 		JComboBox<String>scoreCombox = new JComboBox<String>(score);
 		
-		JTextField text3 =  new  JTextField(20);
-		JTextField text4 =  new  JTextField(10);
+		JTextField text3 =  new  JTextField(8);
+		JTextField text4 =  new  JTextField(2);
 		JComboBox<String>scoreCombox2 = new JComboBox<String>(score);
 		
-		JTextField text5 =  new  JTextField(20);
-		JTextField text6 =  new  JTextField(10);
+		JTextField text5 =  new  JTextField(8);
+		JTextField text6 =  new  JTextField(2);
 		JComboBox<String>scoreCombox3 = new JComboBox<String>(score);
 		
-		JTextField text7 =  new  JTextField(20);
-		JTextField text8 =  new  JTextField(10);
+		JTextField text7 =  new  JTextField(8);
+		JTextField text8 =  new  JTextField(2);
 		JComboBox<String>scoreCombox4 = new JComboBox<String>(score);
 		
-		JTextField text9 =  new  JTextField(20);
-		JTextField text10 =  new  JTextField(10);
+		JTextField text9 =  new  JTextField(8);
+		JTextField text10 =  new  JTextField(2);
 		JComboBox<String>scoreCombox5 = new JComboBox<String>(score);
 		
-		panel.add(new JLabel("과목"));
-		contentpane.add (text1);
-		panel.add(new JLabel("학점"));
-		contentpane.add (text2);
-		panel.add(new JLabel("과목"));
-		contentpane.add (text3);
-		panel.add(new JLabel("학점"));
-		contentpane.add (text4);
-		panel.add(new JLabel("과목"));
-		contentpane.add (text5);
-		panel.add(new JLabel("학점"));
-		contentpane.add (text6);
-		panel.add(new JLabel("과목"));
-		contentpane.add (text7);
-		panel.add(new JLabel("학점"));
-		contentpane.add (text8);
-		panel.add(new JLabel("과목"));
-		contentpane.add (text9);
-		panel.add(new JLabel("학점"));
-		contentpane.add (text10);
+		JTextField text11 =  new  JTextField(5);
+		JTextField text12 =  new  JTextField(3);
 		
+		panel.add(new JLabel("1.과목"));
+		panel.add (text1);
+		
+		panel.add(new JLabel("학점"));
+		panel.add (text2);
+		
+		panel.add(new JLabel("평점"));
+		 panel.add(scoreCombox);
+		 
+		panel.add(new JLabel("2.과목"));
+		panel.add (text3);
+		
+		panel.add(new JLabel("학점"));
+		panel.add (text4);
+		
+		panel.add(new JLabel("평점"));
+		panel.add(scoreCombox2);
+		 
+		panel.add(new JLabel("3.과목"));
+		panel.add (text5);
+		
+		panel.add(new JLabel("학점"));
+		panel.add (text6);
+		
+		panel.add(new JLabel("평점"));
+		panel.add(scoreCombox3);
+		 
+		panel.add(new JLabel("4.과목"));
+		panel.add (text7);
+		
+		panel.add(new JLabel("학점"));
+		panel.add (text8);
+		
+		panel.add(new JLabel("평점"));
+		panel.add(scoreCombox4);
+		 
+		panel.add(new JLabel("5.과목"));
+		panel.add (text9);
+		
+		panel.add(new JLabel("학점"));
+		panel.add (text10);
+		
+		panel.add(new JLabel("평점"));
+		panel.add(scoreCombox5);
+		
+		contentPane.add(panel, BorderLayout.CENTER);
+		
+		panel.add(new JLabel("총 학점"));
+		panel.add (text11);
+		
+		panel.add(new JLabel("평점"));
+		panel.add (text12);
+	
+		
+		
+		 JButton okBtn      =  new  JButton ("계산");
+		 panel.add(okBtn);
+			okBtn.addActionListener( new TestScore_Listener 
+					(text2, text4, text6, text8, text10, text11));
+			
+			contentPane.add(panel, BorderLayout.SOUTH);
+			
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.pack();
+			frame.setVisible(true);
+			frame.setLocationRelativeTo(null);//화면 전중앙 배치
+			
 	}
 
 }
--- 뇌를 자극하는 자바/src/Test/TestScore_Listener.java
+++ 뇌를 자극하는 자바/src/Test/TestScore_Listener.java
@@ -4,19 +4,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class TestScore_Listener implements ActionListener {
 	
-	JTextField text1;
-	JTextField text2;
+	JTextField text1, text2, text3, text4, text5, text6, text7,
+	           text8, text9, text10, text11, text12;
+	
 	
 	
 
-	public TestScore_Listener(JTextField text1, JTextField text2) {
+	public TestScore_Listener(JTextField text2, JTextField text4, JTextField text6, 
+			JTextField text8, JTextField text10, JTextField text11) {
 		super();
-		this.text1 = text1;
 		this.text2 = text2;
+		this.text4 = text4;
+		this.text6 = text6;
+		this.text8 = text8;
+		this.text10 = text10;
+		this.text11 = text11;
+	}
+
+
+
+	public TestScore_Listener() {
+		System.out.println("TestScore_Listener() 기본 생성자 호출");
 	}
 
 
@@ -27,36 +40,49 @@ public class TestScore_Listener implements ActionListener {
 		JButton b= (JButton)e.getSource();
 		if(b.getText().equals("계산")) {
 			
-			//계산
+			//과목 학점 합산
 			
-			String strNum1 = text1.getText();
-			String strNum2 = text2.getText();
+			String strNum1 = text2.getText();
+			String strNum2 = text4.getText();
+			String strNum3 = text6.getText();
+			String strNum4 = text8.getText();
+			String strNum5 = text10.getText();
 			
 			int num1 = Integer.parseInt(strNum1);
 			int num2 = Integer.parseInt(strNum2);
-			int total = num1 + num2;
+			int num3 = Integer.parseInt(strNum3);
+			int num4 = Integer.parseInt(strNum4);
+			int num5 = Integer.parseInt(strNum5);
 			
+			int total = num1 + num2 + num3 + num4 + num5;
+			
+			if(strNum1.equals("")) {
+				JOptionPane.showMessageDialog(null, "학점을 입력해 주세요.", "경고 메세지", 
+						JOptionPane.WARNING_MESSAGE);
+				
 			System.out.println(total);
 			
+			text11.setText(String.valueOf(total));
 			
+			
+							
+	}else {
+			//초기화
+			text1.setText("");
+			text2.setText("");
+			text3.setText("");
+			text4.setText("");
+			text5.setText("");
+			text6.setText("");
+			text7.setText("");
+			text8.setText("");
+			text9.setText("");
+			text10.setText("");
+			text11.setText("");
+			text12.setText("");
 		}
-		public void insertTable(String[] arr) throws Exception {
-			
-			String name = arr[0]; //과목 이름
-			String credit = arr[1]; //학점
-			String Score = arr[2]; // 평점
-			
-		}
-		Score = Score.equals("A+", "A", "B+", "B", "C+", "C", "D+", "D", "F");
-		
-
-	}
-
-
-
-	public void insertTable(String[] arr) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
+}
+			
